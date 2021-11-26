5eec5940fd96e08f431ddc6d4652ff7e063fee85
vinicius <viniciusyamate5@gmail.com>
2021-11-26 18:53:29 -0300

    exercicios 1 e 2

---
+++ fundamentos/bloco4/dia1/3.js
@@ -0,0 +1,16 @@
+const a = 90
+const b = 4
+const c = -90
+
+if (a > b && a > c) {
+    console.log(a);
+}
+else if (b > a && b > c) {
+    console.log(b);
+}
+else if (c > a && c > b) {
+    console.log(c);
+}
+else {
+    console.log("eles são iguais")
+}
