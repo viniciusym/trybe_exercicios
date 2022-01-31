54adc7bf301b8a9ee42a937221c96d31a0fc8694
viniciusyamate <viniciusyamate5@gmail.com>
2022-01-31 18:04:16 +0000

    exercicio 6 dia9.1

--- fundamentos/bloco9/dia1/exercicio 3&4.js
+++ fundamentos/bloco9/dia1/exercicio 3&4.js
@@ -14,7 +14,7 @@ const getPlanet = () => {
 setTimeout(() => getPlanet(), 4000) */
 
 /* 4 - Suponha que você precise simular uma mensagem enviada do robô Curiosity de Marte para a Terra. O Curiosity envia para a Terra a temperatura atual em Marte, gastando um tempo variável de até 5 segundos para que termine o envio. Crie a função sendMarsTemperature , que imprime a temperatura em Marte. */
-function exercicio4 () {
+
 
 const messageDelay = () => Math.floor(Math.random() * 5000);
 
@@ -26,35 +26,6 @@ const getMarsTemperature = () => {
 const sendMarsTemperature = () => {
   let currentTemp = getMarsTemperature();
   setTimeout(() => console.log(`Mars temperature is: ${currentTemp} degree Celsius`), messageDelay())
-}
+};
 
-return sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
-}
-
-/* 6 - Por fim, o robô Curiosity tem uma taxa de sucesso de envio de mensagem de 60% devido ao fato de o robô já estar muito ocupado com outras operações. Logo, adicione na função sendMarsTemperature uma outra callback que contenha as ações a serem tomadas em caso de falha. */
-
-const messageDelay = () => Math.floor(Math.random() * 5000);
-
-const getMarsTemperature = () => {
-  const maxTemperature = 58;
-  return Math.floor(Math.random() * maxTemperature);
-}
-
-const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
-
-const temperatureInFahrenheit = (temperature) =>
-  console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
-
-const greet = (temperature) =>
-  console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
-
-const handleError = (errorReason) =>
-  console.log(`Error getting temperature: ${errorReason}`);
-
-// definição da função sendMarsTemperature...
-
-// imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
-sendMarsTemperature(temperatureInFahrenheit, handleError);
-
-// imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
-sendMarsTemperature(greet, handleError);
+sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
