d2826feee3c342fb34043073cac51cc072347249
viniciusym <viniciusyamate5@gmail.com>
2022-06-07 22:46:15 +0100

    exercicio 1

---
+++ back-end/bloco-22-Introdução-ao-desenvolvimento-Web-com-Node.js/dia22.2/exercicios/exercicio1.js
@@ -0,0 +1,13 @@
+const multiply = (num1, num2, num3) => {
+  const promise = new Promise((resolve, reject) => {
+    if(typeof num1 !== 'number' || typeof num2 !== 'number' || typeof num3 !== 'number') reject(new Error('Informe apenas números'));
+    const result = (num1 + num2) * num3;
+    if(result < 50) reject(new Error('Valor muito baixo'));
+    resolve(result);
+  });
+  return promise;
+}
+
+multiply(20, 10, 12)
+  .then(result => console.log(result))
+  .catch(err => console.log(err));
