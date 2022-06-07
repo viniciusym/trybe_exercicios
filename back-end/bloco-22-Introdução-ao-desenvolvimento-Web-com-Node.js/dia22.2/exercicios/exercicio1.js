e973a878b58df707df21845d2923ff5df0fbabed
viniciusym <viniciusyamate5@gmail.com>
2022-06-07 22:47:41 +0100

    refatora verificaçao dos parametros da funçao do exercicio 1

--- back-end/bloco-22-Introdução-ao-desenvolvimento-Web-com-Node.js/dia22.2/exercicios/exercicio1.js
+++ back-end/bloco-22-Introdução-ao-desenvolvimento-Web-com-Node.js/dia22.2/exercicios/exercicio1.js
@@ -1,6 +1,6 @@
 const multiply = (num1, num2, num3) => {
   const promise = new Promise((resolve, reject) => {
-    if(typeof num1 !== 'number' || typeof num2 !== 'number' || typeof num3 !== 'number') reject(new Error('Informe apenas números'));
+    if(typeof (num1, num2, num3) !== 'number' ) reject(new Error('Informe apenas números'));
     const result = (num1 + num2) * num3;
     if(result < 50) reject(new Error('Valor muito baixo'));
     resolve(result);
