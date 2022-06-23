99a1a1b367df0fc966d9aece6494489fdec73502
viniciusym <viniciusyamate5@gmail.com>
2022-06-23 23:34:06 +0100

    fix: corrije erro na validação dos dados para o cadastro de um novo livro
    ao passar uma string vazia para o campo 'author_id' no body da req o middleware tentaria fazer a busca por autores com o id "" retornando um erro por ser um dado invalido, então na validação do id ao ser passado um valor falsy a busca do autor no banco de dados nao vai acontecer e sim sera retornado o valor 'false'

--- back-end/bloco-23-camada-de-serviço-e-arquiteruta-rest-e-restful/dia23.1/books/src/middlewares/validateNewBook.js
+++ back-end/bloco-23-camada-de-serviço-e-arquiteruta-rest-e-restful/dia23.1/books/src/middlewares/validateNewBook.js
@@ -3,7 +3,7 @@ const { getAuthorById } = require('../author/author');
 const validateNewBook = async (req, res, next) => {
   const { body: { tittle, author_id } } = req;
   const isTittleValid = tittle.length >= 3;
-  const isAuthorValid = !!await getAuthorById(author_id);
+  const isAuthorValid = author_id ? !!await getAuthorById(author_id) : false;
   if(!isAuthorValid || !isTittleValid) {
     return res.status(400).json({ message: 'dados inválidos'});
   };
