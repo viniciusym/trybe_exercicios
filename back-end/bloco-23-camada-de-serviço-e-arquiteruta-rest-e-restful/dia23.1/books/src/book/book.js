669928d9a69eea4c67b6f024a498d0722c792395
viniciusym <viniciusyamate5@gmail.com>
2022-06-23 22:10:46 +0100

    rafactor: refatora funções do arquivo 'book.js'
    refatora as funções que acessam o banco de dados que antes eram funções individuais, para um objeto com todas as funções

--- back-end/bloco-23-camada-de-serviço-e-arquiteruta-rest-e-restful/dia23.1/books/src/book/book.js
+++ back-end/bloco-23-camada-de-serviço-e-arquiteruta-rest-e-restful/dia23.1/books/src/book/book.js
@@ -1,26 +1,22 @@
 const connection = require('../db/connection');
 
-const getAllBooks = async () => {
-  const [books] = await connection.query(
-    'select * from model_example.books;',
-  );
-  return books;
-}
-
-const getBookById = async (bookId) => {
-  const sql = 'select * from model_example.books where id = ?'
-  const [[book]] = await connection.query(sql, [bookId]);
-  return book;
-}
-
-const getBookByAuthorId = async (authorId) => {
-  const sql = 'select * from model_example.books where author_id = ?'
-  const [book] = await connection.query(sql, [authorId]);
-  return book;
-}
-
-module.exports = {
-  getAllBooks,
-  getBookByAuthorId,
-  getBookById
+const modelBooks = {
+  async getAllBooks() {
+    const [books] = await connection.query(
+      'select * from model_example.books;',
+    );
+    return books;
+  },
+  async getBookById(bookId) {
+    const sql = 'select * from model_example.books where id = ?'
+    const [[book]] = await connection.query(sql, [bookId]);
+    return book;
+  },
+  async getBookByAuthorId(authorId) {
+    const sql = 'select * from model_example.books where author_id = ?'
+    const [book] = await connection.query(sql, [authorId]);
+    return book;
+  }
 };
+
+module.exports = modelBooks;
