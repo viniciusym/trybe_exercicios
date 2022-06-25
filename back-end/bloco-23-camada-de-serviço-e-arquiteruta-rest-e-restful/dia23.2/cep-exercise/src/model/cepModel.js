ce0e19f2b2c9d48dcad5ac0a910d1b7dfc8273e9
viniciusym <viniciusyamate5@gmail.com>
2022-06-25 02:52:27 +0100

    feat(models): implementa na camada model função para retornar todos os ceps cadastrados no banco de dados

--- back-end/bloco-23-camada-de-serviço-e-arquiteruta-rest-e-restful/dia23.2/cep-exercise/src/model/cepModel.js
+++ back-end/bloco-23-camada-de-serviço-e-arquiteruta-rest-e-restful/dia23.2/cep-exercise/src/model/cepModel.js
@@ -2,6 +2,8 @@ const connection = require('./db');
 
 const cepModel = {
   async getAll() {
-    const sql = 'select *'
+    const sql = 'select * from cep_lookup.cep';
+    const [ceps] = await connection.query(sql);
+    return ceps;
   }
 }
