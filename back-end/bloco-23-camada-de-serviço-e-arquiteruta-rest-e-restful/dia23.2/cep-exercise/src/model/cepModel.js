44de082927fc61e13bfe51c20d3f87a9c1c24068
viniciusym <viniciusyamate5@gmail.com>
2022-06-25 02:57:58 +0100

    feat(model): implementa novas funções na camada model
    implementa funções para adicionar novo cep, verificar se um numero de cep esta cadastrado no banco de dados e selecionar um cep pelo seu numero

--- back-end/bloco-23-camada-de-serviço-e-arquiteruta-rest-e-restful/dia23.2/cep-exercise/src/model/cepModel.js
+++ back-end/bloco-23-camada-de-serviço-e-arquiteruta-rest-e-restful/dia23.2/cep-exercise/src/model/cepModel.js
@@ -5,5 +5,22 @@ const cepModel = {
     const sql = 'select * from cep_lookup.cep';
     const [ceps] = await connection.query(sql);
     return ceps;
+  },
+  async exists(cepNumber) {
+    const sql = 'select * from cep_lookup.cep where cep = ?';
+    const [[cep]] = await connection.query(sql, [cepNumber]);
+    return !!cep;
+  },
+  async getByNumber(cepNumber) {
+    const sql = 'select * from cep_lookup.cep where cep = ?';
+    const [[cep]] = await connection.query(sql, [cepNumber]);
+    return cep
+  },
+  async insertNew(newCep) {
+    const sql = 'insert into cep_lookup.cep set ?';
+    const [[insertResponse]] = await connection.query(sql, [newCep]);
+    return insertResponse;
   }
-}
+};
+
+module.exports = cepModel;
