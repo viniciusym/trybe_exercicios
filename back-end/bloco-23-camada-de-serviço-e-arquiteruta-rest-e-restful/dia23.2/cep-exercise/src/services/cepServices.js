9c34893d7d826c0a23b26f464f4137cbec759cd0
viniciusym <viniciusyamate5@gmail.com>
2022-06-27 23:24:37 +0100

    feat(service): consulta api externa ao buscar um cep pela rota get e esse cep não existir no banco de dados consulta em api externa esse cep se ele for retornado com sucesso sera adicionado ao banco de dados e retornado para a requisição get, se mesmo na api externa ele não existir dispara um erro

--- back-end/bloco-23-camada-de-serviço-e-arquiteruta-rest-e-restful/dia23.2/cep-exercise/src/services/cepServices.js
+++ back-end/bloco-23-camada-de-serviço-e-arquiteruta-rest-e-restful/dia23.2/cep-exercise/src/services/cepServices.js
@@ -1,10 +1,10 @@
 const InvalidDataError = require('../errors/InvalidDataError');
 const NotFoundError = require('../errors/NotFoundError');
 const AlreadyExistsError = require('../errors/AlreadyExistsError');
-const { getByNumber, exists, insertNew } = require('../model/cepModel');
+const { getByNumber, exists, insertNew, getFromOtherApi } = require('../model/cepModel');
 const Joi = require('joi');
 
-const schema = Joi.object ({
+const schema = Joi.object({
   number: Joi.string().pattern(new RegExp(/\d{5}-?\d{3}/)).required(),
   logradouro: Joi.string().required(),
   bairro: Joi.string().required(),
@@ -16,7 +16,7 @@ const cepServices = {
   validateCepFormat(cep) {
     const cepRegex = /\d{5}-?\d{3}/;
     const isInvalidfomart = cepRegex.test(cep) && cep.length === 9;
-    if(!isInvalidfomart) {
+    if (!isInvalidfomart) {
       throw new InvalidDataError('CEP inválido');
     }
   },
@@ -30,15 +30,40 @@ const cepServices = {
   },
   async checkIfNewCepAlreadyExists(cep) {
     const cepExists = await exists(cep);
-    if(cepExists) {
+    if (cepExists) {
       throw new AlreadyExistsError('CEP já existente');
     }
   },
-  async checkIfExists(cep) {
-    const cepExists = await exists(cep);
-    if(!cepExists) {
+  async insertNewCepWhenNotExists(cep) {
+    const cepData = await getCepFromOtherApi(cep);
+    if (cepData.erro) {
       throw new NotFoundError('CEP não encontrado');
     }
+    return cepData;
+  },
+  async checkIfExists(cep) {
+    const cepExists = await exists(cep);
+    if (!cepExists) {
+      const { data } = await getFromOtherApi(cep);
+      if (data.erro) {
+        throw new NotFoundError('CEP não encontrado');
+      };
+      const { cep: number,
+        logradouro,
+        bairro,
+        localidade,
+        uf } = data;
+      const cepObject = {
+        number,
+        logradouro,
+        bairro,
+        localidade,
+        uf
+      };
+      const { affectedRows }= await insertNew(cepObject)
+      return !!affectedRows;
+    };
+    return cepExists;
   },
   async getByNumber(cep) {
     const cepNumber = cep;
