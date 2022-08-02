19c15783e759cc349844b71df454053cd046913f
viniciusym <viniciusyamate5@gmail.com>
2022-08-02 19:49:44 +0100

    feat: define o setter e o getter para _connectedTo

--- back-end/bloco-27-poo-e-solid/dia27.1-introduçao/exercicio-classe-televisor/Tv.ts
+++ back-end/bloco-27-poo-e-solid/dia27.1-introduçao/exercicio-classe-televisor/Tv.ts
@@ -1,21 +1,34 @@
 class Tv {
-  brand: string;
-  size: number;
-  resolution: number;
-  connection: string[];
-  connectedTo: string;
+  private _brand: string;
+  private _size: number;
+  private _resolution: number;
+  private _connection: string[];
+  private _connectedTo: string;
 
   constructor(
     brand: string, size: number, resolution: number, connection: string[]) {
-      this.brand = brand;
-      this.size = size;
-      this.resolution = resolution;
-      this.connection = connection;
+      this._brand = brand;
+      this._size = size;
+      this._resolution = resolution;
+      this._connection = connection;
   };
 
   turnOn() {
-    console.log(`brand: ${this.brand}, size: ${this.size}, resolution: ${this.resolution}, connection: ${this.connection}`)
+    console.log(`brand: ${this._brand}, size: ${this._size}, resolution: ${this._resolution}, connection: ${this._connection}`)
   }
+
+  public set connectedTo(connectedTo: string) {
+    if(!this._connection.includes(connectedTo)) {
+      console.log('Sorry, connection unavailable!');
+      return;
+    }
+    this._connectedTo = connectedTo;
+  }
+
+  public get connectedTo(): string {
+    return this._connectedTo;
+  }
+  
 }
 
 export default Tv;
--- back-end/bloco-27-poo-e-solid/dia27.1-introduçao/exercicio-classe-televisor/index.ts
+++ back-end/bloco-27-poo-e-solid/dia27.1-introduçao/exercicio-classe-televisor/index.ts
@@ -2,3 +2,5 @@ import Tv from './Tv';
 
 const tv1 = new Tv('sony', 27, 1080, ['hdmi', 'vga', 'rca']);
 tv1.turnOn();
+tv1.connectedTo = 'vga';
+console.log(tv1.connectedTo);
